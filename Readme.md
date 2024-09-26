08506f26633b4ad49687991e9a621af449e10d05
spxlpt126 <ron@sparxitsolutions.com>
2024-09-26 16:47:24 +0530

    Vite development process created

--- Readme.md
+++ Readme.md
@@ -1,7 +1,14 @@
 # Shopify App Boiler Plate
 
  ## Stack used
- [] Node
- [] Express
- [] React
- [] Vite
+ [ ] Node
+ [ ] Express
+ [ ] React
+ [ ] Vite
+
+
+
+## Docs
+### You can refer to this docs to uderstand boiler plate more clearly
+[ ] How vite development server works **https://thenewstack.io/how-to-build-a-server-side-react-app-using-vite-and-express/**
+[ ] How App bridge workds and it's purpose **https://shopify.dev/docs/api/app-bridge**
--- server/index.js
+++ server/index.js
@@ -1,5 +1,37 @@
 import express from "express";
+import fs from "fs"
+import path from 'node:path'
 import "dotenv/config";
 
 
+
+import { createServer as createViteServer } from "vite";
+
+const dev = process.env.NODE_ENV == 'dev' || false;
+
 const app = express();
+
+const createServer = async () =>{
+    const viteServer = await createViteServer({
+        root:path.resolve(process.cwd(),"client"),
+        server:{
+            middlewareMode:true
+        },
+        appType:'custom'
+    });
+    app.use(viteServer.middlewares);
+    app.use('*',async (req,res) =>{
+        const url = req.originalUrl;
+        try{
+            const template = await viteServer.transformIndexHtml(url, fs.readFileSync(path.resolve(process.cwd(),"client","index.html"),"utf-8"));
+            res.status(200).set({'Content-Type':'text/html'}).end(template)
+        }catch(err){
+            console.log("error occured",err)
+            res.status(500).end(err)
+        }
+    })
+    app.listen(8082,() =>{
+        console.log("listening to PORT", process.env.PORT || 8082)
+    })
+}
+createServer();
